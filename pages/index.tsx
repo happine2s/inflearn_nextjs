6e1b50b92f281b10eb40c361d40d802191e1b165
happine2s <happine2s@sookmyung.ac.kr>
2022-11-09 23:46:07 +0900

    포스트 자세히 보기 페이지로 이동(file system 기반의 라우팅)

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { getSortedPostsData } from '../lib/post'
 import homeStyles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 const Home=({allPostsData}:{
   allPostsData:{
@@ -27,7 +28,13 @@ const Home=({allPostsData}:{
         <ul className={homeStyles.list}>
           {allPostsData.map(({id, title, date})=>
           <li className={homeStyles.listItem} key={id}>
-            <a>{title}</a>
+            <Link href={`/posts/${id}`}>
+              {/*
+                a태그인데 에러나서 p태그로 대체
+                Error: Invalid <Link> with <a> child. Please remove <a> or use <Link legacyBehavior>.
+              */}
+              <p>{title}</p>
+            </Link>
             <br/>
             <small className={homeStyles.lightText}>
               {date}
